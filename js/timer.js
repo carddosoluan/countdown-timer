1e78873c84bac10b8ea23eccfeee608bb0d434e1
Luan Cardoso <carddosoluan@gmail.com>
2023-06-09 12:49:06 -0300

    Event listener and the update interface method

---
+++ js/timer.js
@@ -0,0 +1,49 @@
+export default
+
+class Timer {
+    constructor(root) {
+        root.innerHTML = Timer.getHTML();
+
+        this.el = {
+            minutes: root.querySelector(".timer__part--minutes"),
+            seconds: root.querySelector(".timer__part--seconds"),
+            control: root.querySelector(".timer__btn--control"),
+            reset: root.querySelector(".timer__btn--reset")
+        };
+        
+        this.interval = null;
+        this.remainingSeconds = 90;
+
+        this.updateInterfaceTime();
+
+        this.el.control.addEventListener("click", () => {
+            // TODO: add in the code
+        });
+
+        this.el.reset.addEventListener("click", () => {
+            // TODO: add in the code
+        });
+
+    }
+
+    updateInterfaceTime() {
+        const minutes = Math.floor(this.remainingSeconds / 60);
+        const seconds = this.remainingSeconds % 60;
+
+        console.log(minutes, seconds);
+    }
+
+    static getHTML() {
+        return ` 
+            <span class="timer__part timer__part--minutes">00</span>
+            <span class="timer__part">:</span>
+            <span class="timer__part timer__part--seconds">00</span>
+            <button type="button" class="timer__btn timer__btn--control timer__btn--start">
+                <span class="material-icons">play_arrow</span>
+            </button>
+            <button type="button" class="timer__btn timer__btn--control timer__btn--reset">
+                <span class="material-icons">timer</span>
+            </button>
+        `;
+    }
+}
